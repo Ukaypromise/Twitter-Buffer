0076262f9e6438f23e579c535e1a9125ee151db4
Ukaypromise <promiseukaimo@gmail.com>
2023-01-19 08:22:54 -0500

    Setup User model with vakidations

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,5 @@
 class MainController < ApplicationController
     def index
-        flash.now[:notice] = "Login successfull: Welcome to the main page"
-        flash.now[:alert] = "Wrong User login details"
+        
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/about-us', to: 'about#index', as:'about'
+  get 'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registrations#new'
   # Defines the root path route ("/")
   root 'main#index'
   # root "articles#index"
